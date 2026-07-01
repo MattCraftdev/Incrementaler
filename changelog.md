cd55ecc57f07cb13417466a4f1199a96ebd84062
MattCraftDev <jacobchewie01@gmail.com>
2026-07-01 17:54:08 -0400

    V.0.1.0

--- changelog.md
+++ changelog.md
@@ -1,2 +1,4 @@
-July 1st 2026 - Initial Build [0]
+July 1st 2026 - Initial Build [0] 3:33PM
 - Time system implemented
+
+2026/07/01 - []
--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 let knowledge = 0;
 
 
-
-
 // Time System
 let day = 0;
 let year = 14;
@@ -12,10 +10,10 @@ setInterval(() => {
     updateTime();
     document.getElementById("displayDay").innerText = "Day: " + day;
     document.getElementById("displayYear").innerText = "Year: " + year;
-}, 500);
+}, 50);
 
 function updateTime() {
-    if (day >= 365) {
+    if (day > 365) {
         day = 0;
         year += 1;
     };
