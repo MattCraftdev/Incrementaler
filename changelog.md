f4fb725a487e072f1c34b1754eb3d86dc6f74a0a
MattCraftDev <jacobchewie01@gmail.com>
2026-07-02 21:15:17 -0400

    V0.2.1

--- changelog.md
+++ changelog.md
@@ -1,21 +1,13 @@
+Newest versions
 
+2026/07/02 - [0.2.1]
+- Fixed error with depression
 
-
-
-
-
-
-
-
-
-
-
-
-
-2026/07/01 - [0.1.0]
+2026/07/01 - [0.2.0]
 - Added Tabs for each part
 - Added knowledge and made it so you can "birth knowledge"
 - Added story message box on the side
+- Added depression as a mechanic, mainly mood thoughssssssss
 
 July 1st 2026 - Initial Build [0] 3:33PM
 - Time system implemented
--- main.js
+++ main.js
@@ -1,20 +1,54 @@
+// Varibles
 let knowledge = 0;
+let depression = 0;
 let baseIncrease = 1;
+let depressionStatus = null;
 
 
-
+// Knowledge accumilination system
 document.getElementById("createKnowledge").addEventListener("click", () => {
     knowledge += baseIncrease;
     updateKnowledge();
 });
 
 
+
 function updateKnowledge() {
     document.getElementById("displayKnowledge").innerText = "Knowledge: " + knowledge;
-}
+};
+
+// Chatbox creation to send a message
+
+function say(message) {
+    document.getElementById("messageplaceholder").innerText = message;
+};
 
 
+// Depression Mechanics
+setInterval(() => {
+    depression += (knowledge/100-10)
 
+    if (depression>99) {
+        depressionStatus = "Death awaits, calmly."
+        depression = 100;
+    }
+
+    if (depression>50 && depression<99) {
+        depressionStatus = "Sad"
+    }
+
+    if (depression<-50) {
+        depressionStatus = "Happy"
+    }
+    if (depression<-100) {
+        depression = -100;
+        depressionStatus = "Overjoyed";
+    };
+    
+
+    document.getElementById("displayDepression").innerText = "Mood: " + depressionStatus;
+}, 100);
+    
 // Time System
 let day = 0;
 let year = 14;
@@ -24,7 +58,7 @@ setInterval(() => {
     updateTime();
     document.getElementById("displayDay").innerText = "Day: " + day;
     document.getElementById("displayYear").innerText = "Year: " + year;
-}, 50);
+}, 1000);
 
 function updateTime() {
     if (day > 365) {
@@ -32,6 +66,8 @@ function updateTime() {
         year += 1;
     };
 };
+
+
 
 //Tabs
 const TabButtons= document.querySelectorAll(".tab-button");
